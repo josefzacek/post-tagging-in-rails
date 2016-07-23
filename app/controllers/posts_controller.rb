00bc356be555991b65d467235bf7442038d72e04
Josef Zacek <info@josefzacek.com>
2016-07-23 23:56:56 +0100

    filled out post create method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,16 @@ class PostsController < ApplicationController
   end
 
   def create
+    @post = Post.new(post_params)
+    respond_to do |format|
+      if @post.save
+        format.js
+      else
+        format.html { render root_path }
+      end
+    end
+  end
+
   private
 
   def post_params
