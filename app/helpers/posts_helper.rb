354aaabdc1a6aa1bc4516e4c9c1b7cf8d78dfeef
Josef Zacek <info@josefzacek.com>
2016-07-24 21:30:59 +0100

    custom helper to show back/read more button, depending on controller action

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,4 +1,13 @@
 module PostsHelper
+  def post_action_button(action_name, post)
+    case action_name
+    when 'index'
+      link_to 'Read more', post_path(post), class: 'small button'
+    when 'show'
+      link_to 'Posts listings', root_path, class: 'small button'
+    end
+  end
+
   def post_content(action_name, post)
     case action_name
     when 'index'
