4082a45647662072e743e5135351b1ea09a54750
Josef Zacek <info@josefzacek.com>
2016-07-24 21:30:26 +0100

    custom helper to show content, depending on controller action

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,10 @@
 module PostsHelper
+  def post_content(action_name, post)
+    case action_name
+    when 'index'
+      post.content.truncate(177)
+    when 'show'
+      post.content
+    end
+  end
 end
