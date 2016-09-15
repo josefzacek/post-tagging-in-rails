0175a2929d620ed269e3f5a644b717afd89fece5
Josef Zacek <info@josefzacek.com>
2016-09-15 23:56:59 +0100

    created all_tags writer method taking tags from form and inserting them into Tag table if not existing yet

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,10 @@
 class Post < ActiveRecord::Base
   has_many :taggings
   has_many :tags, through: :taggings
+
+  def all_tags=(names)
+    self.tags = names.split(',').map do |name|
+      Tag.where(name: name.strip).first_or_create!
+    end
+  end
 end
