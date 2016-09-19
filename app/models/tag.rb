09e92396aa93d35529d93f7b6160496c7e9ea24a
Josef Zacek <info@josefzacek.com>
2016-09-19 23:17:46 +0100

    pull all tags, group them to remove duplicates and order them by name

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,11 @@
 class Tag < ActiveRecord::Base
   has_many :taggings
   has_many :posts, through: :taggings
+
+  def self.tags
+    select('name')
+      .joins(:taggings)
+      .group('taggings.tag_id')
+      .order('name')
+  end
 end
